3ecf0091b9e934f3ad3cafcc30d5518f42b8fba6
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-09-14 16:20:58 -0700

    This  function that prints 10 times the alphabet, in lowercase, followed by a new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,11 +3,19 @@
 /**
  *main - This is a function that prints 10 times the alphabet, in lowercase, followed by a new line.
  *
- *Return: Always 0.
+ *Return: 0
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char c, i;
 
+	for (i= 0; i <= 9; i++)
+	{
+		for (c = 'a'' c <='z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+	
 }
