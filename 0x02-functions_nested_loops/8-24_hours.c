31351ec016a569ba1d1397bbdb5d9a8f5872a231
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-09-14 21:27:30 -0700

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59..

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,26 +8,19 @@
  */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
 
-	while (hours <= 23)
+	int h, m;
 	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
-	}
-	hours++;
-	minutes = 0;
+		for (h = 0; h < 24; h++)
+		{
+			for (m = 0; m < 60; m++)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10) + '0');
+				_putchar(':');
+				_putchar((m / 10) + '0');
+				_putchar((m % 10) + '0');
+			}	_putchar('\n');
+}
 }
 }
