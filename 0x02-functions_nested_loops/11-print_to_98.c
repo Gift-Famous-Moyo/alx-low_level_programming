9b60b9472bcf1313106aa87d185ed1fc5741920f
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-09-14 19:53:39 -0700

    a function that prints all natural numbers from n to 98, followed by a new line

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_to_98 - This prints all natural numbers from input to 98
- * in order to seperate a comma followed by space.
- * @n: The number to being counting
+ *print_to_98 - This prints all natural numbers from input to 98
+ *	in order to seperate a comma followed by space.
+ *@n: The number to being counting
  */
 void print_to_98(int n)
 {
@@ -11,13 +11,13 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 			printf("%d, ", n--);
-		printf("%d'n", n)
+		printf("%d\n", n)
 	}
 	else
 	{
 		while (n < 98)
 			printf("%d, ", n++);
-		printf("%d\n, n);
+		printf("%d\n", n);
 	}
 }
 
