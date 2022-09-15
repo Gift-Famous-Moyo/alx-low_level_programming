37feb7d9c696ef9985a658ad0e904c9214b1e082
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-09-14 19:23:55 -0700

    a function that prints the 9 times table, starting with 0.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#incude "main.h"
+
+/**
+ *times_table - Prits the 9 times table starting with 0.
+ */
+void tiems_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = i; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
