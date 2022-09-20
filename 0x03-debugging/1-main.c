20603e38f5362d1c2a7dcf03cbce74f1076b3bcd
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-09-20 12:46:14 -0700

    a test for a postitive_or_negative()

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main- causes infinite loop
+ * return: 0
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+int =  0;
+**/
+ *while (i<10)
+ *{
+ *	putchar(i);
+ *}
+ */
+	 printf("infinite loop avoided! \\o/\n");
+ 
+ 	return (0);
+}
