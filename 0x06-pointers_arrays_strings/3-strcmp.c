9891198f6bb2721df9b8d8bd485b75b21ba56180
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-09-21 16:18:50 -0700

    Check ot a t function that compares two stringsfunction that compares two strin
    gs.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include "main.h'
 
 /**
- * _strcmp - compares two strings
- * @s1: the 1st string
- * @s2: the 2nd string
- * 
- * Return: an integer less than, equal to, or greater than
- * zero if s1 less than, equal to, or greater than s2 respectively
- */
+* _strcmp - compares two strings
+* @s1: the 1st string
+* @s2: the 2nd string
+*
+* Return: an integer less than, equal to, or greater than
+* zero if s1 less than, equal to, or greater than s2 respectively
+*/
 
 int _strcmp(char *s1, char *s2)
 {
@@ -22,14 +22,15 @@ int _strcmp(char *s1, char *s2)
 		}
 		else if (s1[i] != s2[i])
 		{
-			res = s1[i] - s2[i];
-			break;
+			res = s1[i] - s2[i]
+			brewak;
 		}
 	}
+
 	if (res < 0)
 		return (res);
 	else if (res == 0)
 		return (0);
 	else
-		return (res);
+		return (0);
 }
