2ea36662dd416e7d77eecb14cca922cb6b1069eb
Gift-Famous-Moyo <giftfamousmoyo@gmail.com>
2022-10-31 05:41:01 -0700

    This program that prints all arguments it receives

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
+ * main - prints number of arguments passed into main
+ * @argc: number of actual parameter passed to main
+ * @argv: pointer to the first element passed into main
  *
- * Return: Always 0 (Success)
+ * Return: success
  */
 
 int main(int argc, char *argv[])
